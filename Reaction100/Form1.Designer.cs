e14baa8c6d9c422965f6b9365e6140b06d580df0
Josiah Stanley <josistan834@ed.amdsb.ca>
2020-05-12 16:28:31 -0400

    Add project files.

---
+++ Reaction100/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Reaction100
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lightOne = new System.Windows.Forms.PictureBox();
+            this.lightTwo = new System.Windows.Forms.PictureBox();
+            this.lightFour = new System.Windows.Forms.PictureBox();
+            this.lightThree = new System.Windows.Forms.PictureBox();
+            this.lightSix = new System.Windows.Forms.PictureBox();
+            this.lightFive = new System.Windows.Forms.PictureBox();
+            this.lightEight = new System.Windows.Forms.PictureBox();
+            this.lightSeven = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.goButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.lightTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.lightOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightFour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightSix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightFive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightEight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightSeven)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lightOne
+            // 
+            this.lightOne.BackColor = System.Drawing.Color.Olive;
+            this.lightOne.Location = new System.Drawing.Point(12, 12);
+            this.lightOne.Name = "lightOne";
+            this.lightOne.Size = new System.Drawing.Size(96, 84);
+            this.lightOne.TabIndex = 0;
+            this.lightOne.TabStop = false;
+            // 
+            // lightTwo
+            // 
+            this.lightTwo.BackColor = System.Drawing.Color.Olive;
+            this.lightTwo.Location = new System.Drawing.Point(128, 12);
+            this.lightTwo.Name = "lightTwo";
+            this.lightTwo.Size = new System.Drawing.Size(96, 84);
+            this.lightTwo.TabIndex = 1;
+            this.lightTwo.TabStop = false;
+            // 
+            // lightFour
+            // 
+            this.lightFour.BackColor = System.Drawing.Color.Olive;
+            this.lightFour.Location = new System.Drawing.Point(128, 113);
+            this.lightFour.Name = "lightFour";
+            this.lightFour.Size = new System.Drawing.Size(96, 84);
+            this.lightFour.TabIndex = 3;
+            this.lightFour.TabStop = false;
+            // 
+            // lightThree
+            // 
+            this.lightThree.BackColor = System.Drawing.Color.Olive;
+            this.lightThree.Location = new System.Drawing.Point(12, 113);
+            this.lightThree.Name = "lightThree";
+            this.lightThree.Size = new System.Drawing.Size(96, 84);
+            this.lightThree.TabIndex = 2;
+            this.lightThree.TabStop = false;
+            // 
+            // lightSix
+            // 
+            this.lightSix.BackColor = System.Drawing.Color.Olive;
+            this.lightSix.Location = new System.Drawing.Point(128, 220);
+            this.lightSix.Name = "lightSix";
+            this.lightSix.Size = new System.Drawing.Size(96, 84);
+            this.lightSix.TabIndex = 5;
+            this.lightSix.TabStop = false;
+            // 
+            // lightFive
+            // 
+            this.lightFive.BackColor = System.Drawing.Color.Olive;
+            this.lightFive.Location = new System.Drawing.Point(12, 220);
+            this.lightFive.Name = "lightFive";
+            this.lightFive.Size = new System.Drawing.Size(96, 84);
+            this.lightFive.TabIndex = 4;
+            this.lightFive.TabStop = false;
+            // 
+            // lightEight
+            // 
+            this.lightEight.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.lightEight.Location = new System.Drawing.Point(128, 325);
+            this.lightEight.Name = "lightEight";
+            this.lightEight.Size = new System.Drawing.Size(96, 84);
+            this.lightEight.TabIndex = 7;
+            this.lightEight.TabStop = false;
+            // 
+            // lightSeven
+            // 
+            this.lightSeven.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.lightSeven.Location = new System.Drawing.Point(12, 325);
+            this.lightSeven.Name = "lightSeven";
+            this.lightSeven.Size = new System.Drawing.Size(96, 84);
+            this.lightSeven.TabIndex = 6;
+            this.lightSeven.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("BIZ UDGothic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(230, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 64);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Reaction:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("BIZ UDGothic", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(273, 113);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(140, 48);
+            this.outputLabel.TabIndex = 9;
+            this.outputLabel.Text = "0.000";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(271, 277);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(241, 59);
+            this.startButton.TabIndex = 10;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(271, 352);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(241, 59);
+            this.goButton.TabIndex = 11;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(271, 201);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(241, 59);
+            this.resetButton.TabIndex = 12;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // lightTimer
+            // 
+            this.lightTimer.Interval = 400;
+            this.lightTimer.Tick += new System.EventHandler(this.lightTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(555, 423);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lightEight);
+            this.Controls.Add(this.lightSeven);
+            this.Controls.Add(this.lightSix);
+            this.Controls.Add(this.lightFive);
+            this.Controls.Add(this.lightFour);
+            this.Controls.Add(this.lightThree);
+            this.Controls.Add(this.lightTwo);
+            this.Controls.Add(this.lightOne);
+            this.Name = "Form1";
+            this.Text = "Can you react at super speeds?";
+            ((System.ComponentModel.ISupportInitialize)(this.lightOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightFour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightSix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightFive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightEight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightSeven)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox lightOne;
+        private System.Windows.Forms.PictureBox lightTwo;
+        private System.Windows.Forms.PictureBox lightFour;
+        private System.Windows.Forms.PictureBox lightThree;
+        private System.Windows.Forms.PictureBox lightSix;
+        private System.Windows.Forms.PictureBox lightFive;
+        private System.Windows.Forms.PictureBox lightEight;
+        private System.Windows.Forms.PictureBox lightSeven;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Timer lightTimer;
+    }
+}
+
---
+++ Reaction100/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Reaction100
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            goButton.Enabled = false;
+        }
+
+        Stopwatch timer = new Stopwatch();
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            goButton.Enabled = true;
+            lightTimer.Enabled = true;
+            startButton.Enabled = false;
+            resetButton.Enabled = false;
+        }
+
+        private void lightTimer_Tick(object sender, EventArgs e)
+        {
+            if (lightOne.BackColor == Color.Olive)
+            {
+                lightOne.BackColor = Color.Yellow;
+                lightTwo.BackColor = Color.Yellow;
+            }
+            else if (lightThree.BackColor == Color.Olive)
+            {
+                lightThree.BackColor = Color.Yellow;
+                lightFour.BackColor = Color.Yellow;
+            }
+            else if (lightFive.BackColor == Color.Olive)
+            {
+                lightFive.BackColor = Color.Yellow;
+                lightSix.BackColor = Color.Yellow;
+            }
+            else if (lightSeven.BackColor == Color.DarkOliveGreen)
+            {
+                lightSeven.BackColor = Color.Lime;
+                lightEight.BackColor = Color.Lime;
+                lightTimer.Enabled = false;
+                timer.Start();
+            }
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = timer.Elapsed;
+            if (timer.ElapsedMilliseconds > 0)
+            {
+                outputLabel.Text = elapsed.ToString(@"s\:fff");
+            }
+            else
+            {
+                outputLabel.Text = "Foul Start";
+            }
+            goButton.Enabled = false;
+            resetButton.Enabled = true;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            lightOne.BackColor = Color.Olive;
+            lightTwo.BackColor = Color.Olive;
+            lightThree.BackColor = Color.Olive;
+            lightFour.BackColor = Color.Olive;
+            lightFive.BackColor = Color.Olive;
+            lightSix.BackColor = Color.Olive;
+            lightSeven.BackColor = Color.DarkOliveGreen;
+            lightEight.BackColor = Color.DarkOliveGreen;
+            startButton.Enabled = true;
+            goButton.Enabled = false;
+            timer.Reset();
+            outputLabel.Text = "0.00";
+        }
+    }
+}
